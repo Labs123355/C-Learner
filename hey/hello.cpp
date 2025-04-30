7614aacd8be7c8e0feba7842b25f50b74c697901
yasuke123 <avi5117@psu.edu>
2025-04-30 12:49:52 -0400

    save

--- hey/hello.cpp
+++ hey/hello.cpp
@@ -3,5 +3,5 @@
 int main()
 {
     std::cout << "Hello World" ; // character output - display "Hello World"
-    return 0; // everything is ok i.e retur
+    return 0; // everything is ok i.e return
 }
