97bc05b56e8dcb3c73b197f1946e76fc8a77cd53
yasuke123 <afolabiisiaka22@gmail.com>
2025-06-09 18:16:47 -0400

    3.51.34 lesson 1

---
+++ gameDesign/inheritance.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+// we want to pass a & or * so we actually get the memory adrees
+void pp(int & i)  
+{    // give me the memory address where stored
+    std::cout << &i << " " << i << " " << sizeof(i) << "\n"; 
+}
+template <typename T> 
+
+class DynamicArray
+{
+    T * m_arr;
+    size_t m_size;
+
+public: 
+    DynamicArray(size_t size)
+        : m_size(size), m_arr (new T[size])
+    {     
+        std::cout << "Array Constructor\n";  
+    }
+
+    int get(size_t index) const 
+    {
+        return m_arr[index] ;
+    }
+
+    void set(size_t index, T val)
+    {
+        m_arr[index] = val;
+    }
+    void print() const
+    {
+        for (size_t i=0; i<m_size; i++)
+        {
+            std::cout << i << " " << m_arr[i] << "\n";
+        }
+    }
+
+};
+
+int main (int argc, char * argv[])
+{
+
+    DynamicArray<float> myArray(10); 
+
+    myArray.set(4, 7.14); 
+    myArray.set(2, 134.420); 
+
+    myArray.print(); 
+
+    return 0; 
+};
--- gameDesign/introLesson/intro.cpp
+++ gameDesign/introLesson/intro.cpp
@@ -27,26 +27,26 @@ public: // safe-to-use buttons and functions (other code outside the class can a
     */
         : m_first (first), m_last (last), m_id (id) , m_avg (avg) // i.e m_first = first
     {
+ 
+    }
+    int getAvg() const 
+    {
+        return m_avg ;
+    }
+    int getID() const
+    {
+        return m_id ; 
+    }
+    std::string getFirst() const
+    {
+        return m_first ; 
+    }
+    std::string getLast() const 
+    {
+        return m_last ;
+    }
 
-    }
-    int getAvg()
-    {
-        return m_avg;
-    }
-    int getID()
-    {
-        return m_id; 
-    }
-    std::string getFirst()
-    {
-        return m_first; 
-    }
-    std::string getLast()
-    {
-        return m_last;
-    }
-
-    void print()
+    void print() const //I have a function called print that will never change the object
     {
         std::cout << m_first << " " << m_last << " "; 
         std::cout << m_id << " " << m_avg <<"\n";
@@ -89,14 +89,49 @@ void vecTest()
         std::cout << a << "\n";
     }
 };
+
+class Course 
+{   // gonna have a course name 
+    std::string m_name = "Course";
+    // vector of students  
+    std::vector<Student> m_students;
+public: 
+   Course () {} // default contructor 
+   // class type then modifier 
+   Course(const std::string& name ) // ref tp string lets us chg lets + const
+   // before we step into what function does  initialize 
+   : m_name(name)
+   {
+   }	   
+   void addStudent(const Student& s)
+   {
+       m_students.push_back(s);
+   }	   
+   std::vector<Student> getStudents() const
+   {
+	return m_students;      	   
+   }	   
+   void print() const
+   {
+   	for (auto& s : m_students)
+   	{
+	    s.print();	
+   	}	   
+   }
+};
+
 void school()
 {
     Student s1; 
     Student s2("Dave", "Churchhill", 1, 3.14); 
     const Student s3("Jane", "Doe", 2022000001, 99.9); 
-
-    s3.print();
-
+    
+    Course comp4300("Comp 4300"); // create a course 
+    comp4300.addStudent(s1); 
+    comp4300.addStudent(s2);
+    comp4300.addStudent(s3);
+    comp4300.addStudent(Student("Billy", "Bob", 3, 50.0));
+    comp4300.print();    
 };
 
 
